8be486ddbb1bb846c56bc4ce9851007e6c30a1b0
Prelang Builder <builder@prelang.com>
2014-03-14 19:05:30 +0000

    Generated scaffolding for model via Rails

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 LinkAggregator::Application.routes.draw do
+  resources :tags
+
   devise_for :users, controllers: {registrations: "users/registrations", sessions: "users/sessions", passwords: "users/passwords"}, skip: [:sessions, :registrations]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
